a55868345bbb224f5cc389ccf3c8591999fba74c
Grewather <user@user.com>
2024-06-28 18:02:52 +0200

    buggy water

--- mods/lkxv_mapgen/init.lua
+++ mods/lkxv_mapgen/init.lua
@@ -1,11 +1,12 @@
 lkxv_spawn = vector.new(0, 0, 0)
 
 minetest.register_alias("mapgen_stone", "lkxv_nodes:grass")
-minetest.register_alias("mapgen_water_source", "lkxv_nodes:dirt")
+minetest.register_alias("mapgen_water_source", "lkxv_nodes:water")
 minetest.register_alias("mapgen_lava_source", "lkxv_nodes:dirt")
 minetest.register_alias("mapgen_dirt", "lkxv_nodes:dirt")
 minetest.register_alias("mapgen_dirt_with_grass", "lkxv_nodes:grass")
 minetest.register_alias("mapgen_sand", "lkxv_nodes:dirt")
+
 
 minetest.register_alias("mapgen_tree", "lkxv_nodes:dirt")
 minetest.register_alias("mapgen_leaves", "lkxv_nodes:dirt")
@@ -17,7 +18,8 @@ local function find_spawn()
     local y
     local x
     local z
-    repeat -- znowu patrz
+
+    repeat 
         if range >= 4000 then
             return vector.new(0,0,0)
         end
--- mods/lkxv_nodes/init.lua
+++ mods/lkxv_nodes/init.lua
@@ -16,3 +16,96 @@ minetest.register_node("lkxv_nodes:grass", {
     diggable = true,
 	groups = { oddly_breakable_by_hand = 1 },
 })
+
+
+minetest.register_node("lkxv_nodes:water", {
+    description = "Water",
+    drawtype = "liquid",
+    tiles = {
+        {
+            name = "lkxv_water_source.png",
+            backface_culling = false,
+            animation = {
+                type = "vertical_frames",
+                aspect_w = 16,
+                aspect_h = 16,
+                length = 2.0,
+            },
+        },
+    },
+    special_tiles = {
+        {
+            name = "lkxv_water_source.png",
+            backface_culling = false,
+            animation = {
+                type = "vertical_frames",
+                aspect_w = 16,
+                aspect_h = 16,
+                length = 2.0,
+            },
+        },
+        {
+            name = "lkxv_water_source.png",
+            backface_culling = true,
+        },
+    },
+    alpha = 160,
+    paramtype = "light",
+    walkable = false,
+    pointable = false,
+    diggable = false,
+    buildable_to = true,
+    is_ground_content = false,
+    liquidtype = "source",
+    liquid_alternative_flowing = "lkxv_nodes:water",
+    liquid_alternative_source = "lkxv_nodes:water",
+    liquid_viscosity = 1,
+    liquid_renewable = false,
+    liquid_range = 2,
+    post_effect_color = {a = 103, r = 30, g = 60, b = 90},
+    groups = {water = 3, liquid = 3, cools_lava = 1},
+})
+
+minetest.register_node("lkxv_nodes:water", {
+    description = "Flowing Water",
+    drawtype = "flowingliquid",
+    tiles = {"lkxv_water_source.png"},
+    special_tiles = {
+        {
+            name = "lkxv_water_source.png",
+            backface_culling = false,
+            animation = {
+                type = "vertical_frames",
+                aspect_w = 16,
+                aspect_h = 16,
+                length = 0.8,
+            },
+        },
+        {
+            name = "lkxv_water_source.png",
+            backface_culling = true,
+            animation = {
+                type = "vertical_frames",
+                aspect_w = 16,
+                aspect_h = 16,
+                length = 0.8,
+            },
+        },
+    },
+    alpha = 160,
+    paramtype = "light",
+    paramtype2 = "flowingliquid",
+    walkable = false,
+    pointable = false,
+    diggable = false,
+    buildable_to = true,
+    is_ground_content = false,
+    liquidtype = "flowing",
+    liquid_alternative_flowing = "lkxv_nodes:water",
+    liquid_alternative_source = "lkxv_nodes:water",
+    liquid_viscosity = 1,
+    liquid_renewable = false,
+    liquid_range = 2,
+    post_effect_color = {a = 103, r = 30, g = 60, b = 90},
+    groups = {water = 3, liquid = 3, cools_lava = 1, not_in_creative_inventory = 1},
+})
